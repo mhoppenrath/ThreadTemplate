b82ba5cfc5f1d7a805af4330cfcc130f640cc703
Michael Hoppenrath <mhoppenrath@truckmovers.com>
2018-11-26 12:07:01 -0600

    First templates and proof of concept

--- assets/functionality.js
+++ assets/functionality.js
@@ -4,9 +4,29 @@ $(document).ready(function() {
           console.log("clicked");
           var driverName = $("#drivername").val().trim();
           var loadType = $("#loadtype").val().trim();
+          var recuiter = $("#recuiter").val().trim();
+          var singleint = $("#singleint").val().trim();
+          var singleDate = $("#singleDate").val().trim();
+          var location = $("#location").val().trim();
+          var company = $("#company").val().trim();
+          var test = $("#test").val().trim();
+          var username = $("#username").val().trim();
+          var password = $("#password").val().trim();
+          var status = $("#status").val().trim();
+          var phone = $("#phone").val().trim();
           event.preventDefault();
+          console.log(driverName);
           $('.write-driver').text(driverName)
           $('.write-type').text(loadType)
-          
+          $('.write-recuiter').text(recuiter)
+          $('.write-singleint').text(singleint)
+          $('.write-singleDate').text(singleDate)
+          $('.write-location').text(location)
+          $('.write-company').text(company)
+          $('.write-test').text(test)
+          $('.write-username').text(username)
+          $('.write-password').text(password)
+          $('.write-status').text(status)
+          $('.write-phone').text(phone)
     })
 })
